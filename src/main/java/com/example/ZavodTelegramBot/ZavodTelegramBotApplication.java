0fb57f20f7e4e15ba35ab226dd89f41f36a1db89
refff <fedorv59@gmail.com>
2025-05-06 16:03:35 +0300

    add telegram dependency and create bot class

---
+++ src/main/java/com/example/ZavodTelegramBot/ZavodTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.ZavodTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZavodTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZavodTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ZavodTelegramBot/ZavodTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZavodTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZavodTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
